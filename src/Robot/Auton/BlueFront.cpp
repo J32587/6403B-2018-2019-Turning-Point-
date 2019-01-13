28952836130da786c6a29256c4cc4256b7955d59
J32587 <jacob32587@gmail.com>
2019-01-13 14:30:31 -0500

    Almost done with Blue Front(7 points rn, also compiler acting weird)

--- src/Robot/Auton/BlueFront.cpp
+++ src/Robot/Auton/BlueFront.cpp
@@ -2,9 +2,41 @@
 #include "../Motors.h"
 #include "../functions.hpp"
 #include "BlueFront.hpp"
-
+const short unsigned int _autonSpeed = 100;
+Timer BLUEFRONT;
+BLUEFRONT.reset();
 void BlueFront(void)
 {
+  Task reload(DRAWBACK_AUTON_TASK);
+  Intake.move(90);
+  moveRobotPID("north",40,200,_autonSpeed);
+  delay(290);
+  Intake.move(0);
+  moveRobotPID("south",39,250,_autonSpeed);
+  delay(250);
+  moveRobotPID("right turn",97,230,_autonSpeed);
+  delay(250);
+  moveRobotPID("north",10.4,250,80);
+  shoot(false);
+  delay(550);
+  Intake.move(100);
+  moveRobotPID("north",22,250,_autonSpeed);
+  shoot(false);
+  delay(550);
+  moveRobotPID("east",3.2,250,_autonSpeed);
+  delay(250);
+  moveRobotPID("north",17,250,_autonSpeed);
+  delay(250);
+  moveRobotPID("south",22,250,_autonSpeed);
+  delay(250);
+  moveRobotPID("right turn",97,230,_autonSpeed);
+  delay(250);
+  moveRobotPID("south",13,250,_autonSpeed);
+  delay(150);
+  Lift.move(-127);
+  delay(250);
+  Lift.move(0);
 
+  //delay(250);
 
 }
--- src/Robot/Auton/RedFront.cpp
+++ src/Robot/Auton/RedFront.cpp
@@ -6,6 +6,25 @@
 
 void RedFront(void)
 {
+const short unsigned int _autonSpeed = 100;
+Task reload(DRAWBACK_AUTON_TASK);
+moveRobotPID("left turn",97,250,_autonSpeed);
+delay(250);
+moveRobotPID("north",40,250,_autonSpeed);
+delay(250);
+Intake.move(100);
+delay(400);
+moveRobotPID("south",43,250,_autonSpeed);
+delay(250);
+moveRobotPID("right turn",97,250,_autonSpeed);
+delay(250);
+moveRobotPID("north",3,250,50);
+shoot(true);
+delay(250);
+moveRobotPID("north",28,250,_autonSpeed);
+shoot(false);
+moveRobotPID("south",28,250,_autonSpeed);
+delay(250);
 
 
 }
